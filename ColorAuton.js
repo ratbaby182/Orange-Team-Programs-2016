4800ad1824ebd8563074e92a15632fdc953d0820
ratbaby182 <minisculelexicon@gmail.com>
2016-10-18 16:43:08 -0500

    Color Stuff
    Changed the color sensor programs to try to establish a threshold. Never
    got around to testing the most recent version.

---
+++ ColorAuton.js
@@ -0,0 +1,24 @@
+alicorn.shard("ColorAuton")
+
+	.create(function(self) {
+
+		var sdk = self.require("AndroidFTC");
+		var sjhd = 0;
+
+	})
+
+	.update(function(self) {
+
+		for (var i = 0; i < 1; i++) {
+
+			console.log(sdk.get("color1"));
+
+		}
+
+	})
+
+	.destroy(function(self) {
+
+		//whaddup
+
+	});
