4ee67db47daec3fe93dbc68d2e4ec88e17623e5d
ratbaby182 <minisculelexicon@gmail.com>
2016-11-15 16:33:55 -0600

    Progress
    Made a copy of auton_fates to mess around with,but didn't get around to
    it.

---
+++ auton_the_last_one_for_real.js
@@ -0,0 +1,49 @@
+var sdk;
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+alicorn.shard("auton_the_last_one_for_real")
+
+  .create(function(self) {
+
+    sdk = self.require("AndroidFTC");
+
+  })
+
+  .update(function(self) {
+
+    //fire that crossbow whaddup
+    sleep(1000);
+    sdk.set("release", 0);
+    sleep(1000);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //what's up lets hit that ball
+
+    //to the middle bit
+  	sdk.set("left", -.5);
+  	sdk.set("right", .5);
+    sleep(3000);
+
+  	sdk.set("left", 0);
+  	sdk.set("right", 0);
+    sleep(25000);
+
+  })
+
+  .destroy(function(self) {
+
+    sdk.set("crank", 0);
+    sdk.set("release", 0);
+    sdk.set("left", 0);
+    sdk.set("right", 0);
+
+  });
